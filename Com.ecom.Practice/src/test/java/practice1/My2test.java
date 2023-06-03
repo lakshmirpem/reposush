2c38a80c356a0ace3357e550ae1fc18655d30436
lakshmirpem <lakshmirpem20@gmail.com>
2023-06-03 12:12:16 +0530

    commit gh

---
+++ Com.ecom.Practice/src/test/java/practice1/My2test.java
@@ -0,0 +1,12 @@
+package practice1;
+import org.testng.annotations.Test;
+
+
+public class My2test 
+{
+@Test
+public void m() 
+{
+ System.out.println("hi");	
+}
+}
---
+++ Com.ecom.Practice/src/test/java/practice1/my1test.java
@@ -0,0 +1,15 @@
+package practice1;
+
+import org.testng.annotations.Test;
+
+public class my1test
+{
+@Test
+public void show() 
+{
+	System.out.println("hiijava");
+	System.out.println("hello");
+		
+	
+}
+}
